e02c59f36aa79ba7c7d721011ca666962456bd18
zchah001 <72586266+zchah001@users.noreply.github.com>
2020-12-06 17:01:53 -0800

    Update codon_compute.py

--- codon_compute.py
+++ codon_compute.py
@@ -25,10 +25,11 @@ def returnSum(myDict):
         sum = sum + myDict[i]
 
     return sum
-url1="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/salmonella_enterica_subsp_enterica_serovar_typh$
-url2="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/mycobacterium_tuberculosis_h37rv/cds/Mycobacter$
+url1="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2/cds/Salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2.ASM694v2.cds.all.fa.gz"
+url2="ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/mycobacterium_tuberculosis_h37rv/cds/Mycobacterium_tuberculosis_h37rv.ASM19595v2.cds.all.fa.gz"
 file1="Salmonella_enterica_subsp_enterica_serovar_typhimurium_str_lt2.ASM694v2.cds.all.fa.gz"
 file2="Mycobacterium_tuberculosis_h37rv.ASM19595v2.cds.all.fa.gz"
+
 
 if not os.path.exists(file1):
     os.system("curl -O %s"%(url1))
